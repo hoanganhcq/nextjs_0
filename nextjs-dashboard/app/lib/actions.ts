308a3623767b9c5f899473aef98015efd4f7f52d
hoanganhcq <hoanganhcq@gmail.com>
2026-09-13 00:25:47 +0700

    Chapter 14: Adding Authentication

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -9,6 +9,9 @@ import { z } from 'zod'; // a TypeScript-first validation library
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const FormSchema = z.object({
     id: z.string(),
 
@@ -136,4 +139,25 @@ export async function deleteInvoice(id: string) {
         DELETE FROM invoices WHERE id = ${id}
     `;
     revalidatePath('/dashboard/invoices');
+}
+
+
+// Chapter 14: Authentication
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
